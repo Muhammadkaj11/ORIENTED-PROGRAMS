ebf73f6563239510c377054f9ddec958e37ae2a3
Muhammadkaj11 <muhdkaj11@gmail.com>
2024-12-20 21:24:09 +0100

    initial commit

---
+++ op.py
@@ -0,0 +1,10 @@
+class parrot:
+    species="bird"
+    def __init__(self,name,age):
+       self.name=name
+       self.age=age
+obj=parrot("shutup",67)
+print(obj.species)
+print(obj.name)
+print(obj.age)
+        
